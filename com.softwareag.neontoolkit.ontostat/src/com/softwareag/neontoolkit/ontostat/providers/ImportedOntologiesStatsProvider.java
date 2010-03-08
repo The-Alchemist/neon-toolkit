f667c012e67e7db1facb9a888aa261c2d62ae027
wwa <wwa@641896ee-dca0-4984-aaae-e2f8ba423eba>
2010-03-08 10:35:27 +0000

    ontostat plugin

---
+++ com.softwareag.neontoolkit.ontostat/src/com/softwareag/neontoolkit/ontostat/providers/ImportedOntologiesStatsProvider.java
@@ -0,0 +1,38 @@
+package com.softwareag.neontoolkit.ontostat.providers;
+
+import org.eclipse.swt.graphics.Image;
+import org.semanticweb.owlapi.model.OWLOntology;
+
+import com.ontoprise.ontostudio.owl.gui.OWLPlugin;
+import com.ontoprise.ontostudio.owl.gui.OWLSharedImages;
+import com.ontoprise.ontostudio.owl.model.OWLModel;
+import com.softwareag.neontoolkit.ontostat.StatsProvider;
+
+public class ImportedOntologiesStatsProvider extends StatsProvider {
+
+	public Object getValue(OWLModel model) {
+		try {
+			return model.getImportedOntologiesURIs().size();
+		} catch (Exception e) {
+			return 0;
+		}
+	}
+	
+	public OWLOntology[] getElements(OWLModel model) {
+		try {
+			return model.getOntology().getImports().toArray(new OWLOntology[0]);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+
+	public Class getType() {
+		return OWLOntology.class;
+	}
+
+	public Image getIconImage() {
+		return OWLPlugin.getDefault().getImageRegistry().get(OWLSharedImages.ONTOLOGY);
+	}
+	
+}
