662322db7ec4475e95fe25a6f29d1967d51e4c66
emwiemaikel <emwiemaikel@641896ee-dca0-4984-aaae-e2f8ba423eba>
2010-09-02 12:01:29 +0000

    NTK2.3.2 release

---
+++ branches/NTK_2.3.2/org.neontoolkit.swt/src/main/org/neontoolkit/table/celleditors/ObjectComboBoxCellEditor.java
@@ -0,0 +1,60 @@
+/*****************************************************************************
+ * Copyright (c) 2009 ontoprise GmbH.
+ *
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+
+package org.neontoolkit.table.celleditors;
+
+import org.eclipse.jface.viewers.ComboBoxCellEditor;
+import org.eclipse.swt.widgets.Composite;
+
+public class ObjectComboBoxCellEditor extends ComboBoxCellEditor {
+	private Object[] _objects;
+	private IComboBoxContentHandler _handler;
+
+	public ObjectComboBoxCellEditor(Composite composite, IComboBoxContentHandler handler) {
+		super(composite, new String[0]);
+		_handler = handler;
+	}
+
+	public Object[] getObjectItems() {
+		if (_objects == null) {
+			_objects = _handler.getElements();
+			String[] items = new String[_objects.length];
+			for (int i=0; i<_objects.length; i++) {
+				items[i] = _handler.toString(_objects[i]);
+			}
+			setItems(items);
+		}
+		return _objects;
+	}
+	
+	@Override
+	protected void doSetValue(Object value) {
+		Object[] objectItems = getObjectItems();
+        if (value instanceof String[]) {
+            for (int i = 0; i < objectItems.length; i++) {
+                if (objectItems[i] == value) {
+                    super.doSetValue(new Integer(i));
+                }
+            }
+        } else {
+            super.doSetValue(value);
+        }
+	}
+	
+	public void clearObjectItems() {
+		_objects = null;
+	}
+	
+	@Override
+	public String[] getItems() {
+		getObjectItems();
+		return super.getItems();
+	}
+}
