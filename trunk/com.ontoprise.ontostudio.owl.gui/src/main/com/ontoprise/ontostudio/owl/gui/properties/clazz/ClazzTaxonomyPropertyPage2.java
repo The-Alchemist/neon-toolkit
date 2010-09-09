3171924d7a6f33238ae4dc6f3fcb47b403349710
nicostieler <nicostieler@641896ee-dca0-4984-aaae-e2f8ba423eba>
2010-09-09 14:33:29 +0000

    bug fix: bug 415 (1/2) Provide default datatype

--- trunk/com.ontoprise.ontostudio.owl.gui/src/main/com/ontoprise/ontostudio/owl/gui/properties/clazz/ClazzTaxonomyPropertyPage2.java
+++ trunk/com.ontoprise.ontostudio.owl.gui/src/main/com/ontoprise/ontostudio/owl/gui/properties/clazz/ClazzTaxonomyPropertyPage2.java
@@ -607,7 +607,7 @@ public class ClazzTaxonomyPropertyPage2 extends AbstractOWLIdPropertyPage {
      * @throws NeOnCoreException
      */
     private void createDisjointOrEquivalentRow(Composite parent, final OWLClassExpression description, final List<LocatedAxiom> axioms, boolean imported, boolean enabled, final int mode, String sourceOnto) throws NeOnCoreException {
-        FormRow row = new FormRow(_toolkit, parent, NUM_COLS, imported, sourceOnto,_owlModel.getProjectId(),_id);//NICO changed
+        FormRow row = new FormRow(_toolkit, parent, NUM_COLS, imported, sourceOnto,_owlModel.getProjectId(),_id);
 
         final String[] descriptionArray = getArrayFromDescription(description);
         String id = OWLGUIUtilities.getEntityLabel(descriptionArray);
