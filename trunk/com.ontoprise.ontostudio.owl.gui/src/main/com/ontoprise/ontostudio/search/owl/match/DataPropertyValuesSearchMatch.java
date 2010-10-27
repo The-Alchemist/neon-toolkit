4173aa3790e20ee61d8e431dbdcf19a0d63d973c
nicostieler <nicostieler@641896ee-dca0-4984-aaae-e2f8ba423eba>
2010-10-27 08:29:20 +0000

    Bug fixed bug# 444
    search result style changed for the tree result view

--- trunk/com.ontoprise.ontostudio.owl.gui/src/main/com/ontoprise/ontostudio/search/owl/match/DataPropertyValuesSearchMatch.java
+++ trunk/com.ontoprise.ontostudio.owl.gui/src/main/com/ontoprise/ontostudio/search/owl/match/DataPropertyValuesSearchMatch.java
@@ -156,8 +156,12 @@ public class DataPropertyValuesSearchMatch extends OwlSearchMatch {
         if(end != _value.length()) {
             value += "..."; //$NON-NLS-1$
         }
-        
-        return subject + ": " + prop +" = "+ value + com.ontoprise.ontostudio.owl.gui.Messages.DataPropertyValuesSearchMatch_0 + element.getOntologyUri() + com.ontoprise.ontostudio.owl.gui.Messages.DataPropertyValuesSearchMatch_1 + element.getProjectName() + "]  "; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+
+        String out = subject + ": " + prop +" = "+ value; //$NON-NLS-1$ //$NON-NLS-2$
+//        + com.ontoprise.ontostudio.owl.gui.Messages.DataPropertyValuesSearchMatch_0 + element.getOntologyUri() + com.ontoprise.ontostudio.owl.gui.Messages.DataPropertyValuesSearchMatch_1 + element.getProjectName() + "]  "; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        System.out.println(out);
+        return subject + ": " + prop +" = "+ value; //$NON-NLS-1$ //$NON-NLS-2$
+//       + com.ontoprise.ontostudio.owl.gui.Messages.DataPropertyValuesSearchMatch_0 + element.getOntologyUri() + com.ontoprise.ontostudio.owl.gui.Messages.DataPropertyValuesSearchMatch_1 + element.getProjectName() + "]  "; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
     }
 
 
