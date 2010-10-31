ccaf975fb599536568fe156d57d46fd1fe7fc242
emwiemaikel <emwiemaikel@641896ee-dca0-4984-aaae-e2f8ba423eba>
2010-10-31 12:25:22 +0000

    added all needed features for a complete NTK product

--- trunk/org.neontoolkit.application/src/org/neontoolkit/application/intro/ApplicationWorkbenchWindowAdvisor.java
+++ trunk/org.neontoolkit.application/src/org/neontoolkit/application/intro/ApplicationWorkbenchWindowAdvisor.java
@@ -1,6 +1,9 @@
 package org.neontoolkit.application.intro;
 
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.IWorkbenchPreferenceConstants;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
@@ -23,5 +26,8 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 		configurer.setShowPerspectiveBar(true);
         configurer.setShowStatusLine(false);
         configurer.setTitle(Messages.ApplicationWorkbenchWindowAdvisor_ApplicationTitle);
+        
+		IPreferenceStore apiStore = PlatformUI.getPreferenceStore();
+		apiStore.setDefault(IWorkbenchPreferenceConstants.DOCK_PERSPECTIVE_BAR, "TOP_RIGHT"); 
     }
 }
