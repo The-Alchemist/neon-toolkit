623727935e4e21cf1a96f02e4a8b0a81b25b64c7
nicostieler <nicostieler@641896ee-dca0-4984-aaae-e2f8ba423eba>
2010-11-19 15:34:20 +0000

    changes for a better release: - menu - ...

--- trunk/org.neontoolkit.application/src/org/neontoolkit/application/intro/Application.java
+++ trunk/org.neontoolkit.application/src/org/neontoolkit/application/intro/Application.java
@@ -1,6 +1,9 @@
 package org.neontoolkit.application.intro;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 
@@ -8,10 +11,12 @@ import org.apache.log4j.PropertyConfigurator;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.osgi.service.datalocation.Location;
+import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
-
 /**
  * @author Michael Erdmann
  * This class controls all aspects of the application's execution
@@ -23,8 +28,10 @@ public class Application implements IApplication {
 	 */
 	public Object start(IApplicationContext context) {
         setupLog4j();
+        
+    	Display display = PlatformUI.createDisplay();
 
-		Display display = PlatformUI.createDisplay();
+//		setWorkspaceWithDialog(display);
 		try {
 			int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
 			if (returnCode == PlatformUI.RETURN_RESTART) {
@@ -35,6 +42,106 @@ public class Application implements IApplication {
 			display.dispose();
 		}
 	}
+
+
+
+	/**
+	 * setWorkspaceWithDialog opens a directory dialog to let the user choose his workspace
+	 * 
+	 * @param display - input display
+	 */
+	private void setWorkspaceWithDialog(Display display) {
+    	Location instanceLoc = Platform.getInstanceLocation();
+    	URL url = promptForInstanceLoc(display);
+    	if (url != null) {
+    		try {
+    			instanceLoc.set(url, true);
+    		} catch (Exception e) {
+    			e.printStackTrace();
+    		}
+    	}
+		
+	}
+
+	/**
+	 * setWorkspaceWithDialogWithOuputAndRestriction opens a directory dialog to let the user choose his workspace, with restrictions
+	 * 
+	 * @param display - input display
+	 */
+	private void setWorkspaceWithDialogWithOuputAndRestriction(Display display) {
+	
+		String fileName = "C:\\Users\\nis\\out.txt";
+		if( ! (new File(fileName)).exists()){
+	
+	    	BufferedWriter out = null;
+	    	Location instanceLoc = Platform.getInstanceLocation();
+	    	try{
+	    	    // Create file 
+	    	    FileWriter fstream = new FileWriter(fileName);
+	    	    out = new BufferedWriter(fstream);
+	    	    out.write("TEST");
+	    	    out.write("\n");
+	    	    if(instanceLoc.isSet()){
+	        	    out.write("true");
+	    	    }else{
+	        	    out.write("false");
+	    	    }
+		    }catch (Exception e){
+		    	//nothing to do
+		    }
+	    	URL url = promptForInstanceLoc(display);
+	    	try{
+	    	    out.write(url.toString());
+	    	    out.write("\n");
+		    }catch (Exception e){
+		    	//nothing to do
+		    }
+	
+	    	if (url != null) {
+	    		try {
+	    			instanceLoc.set(url, true);
+	    		} catch (Exception e) {
+	    			e.printStackTrace();
+	    	    	try{
+	    	    	    out.write("Problem");
+	    	    	    out.write("\n");
+	    		    }catch (Exception e2){
+	    		    	//nothing to do
+	    		    }
+	    		}
+	    	}
+	    	try{
+	    	    out.write("TEST");
+	    	    out.write("\n");
+	    	    out.close();
+		    }catch (Exception e){
+		    	//nothing to do
+		    }
+		}
+		
+	}
+
+
+
+
+/*
+ * PRIVATE HELP METHODS to remove elements from the GUI
+ */
+private URL promptForInstanceLoc(Display display) {
+       Shell shell = new Shell(display);
+       DirectoryDialog dialog = new DirectoryDialog(shell);
+       dialog.setText("Select Workspace Directory");
+       dialog.setMessage("Select the workspace directory to use.");
+       String dir = dialog.open();
+       shell.dispose();
+       try {
+           return dir == null ? null : new File(dir).toURL();
+       } catch (MalformedURLException e) {
+           return null;
+       }
+   }
+
+
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.equinox.app.IApplication#stop()
--- trunk/org.neontoolkit.application/src/org/neontoolkit/application/intro/ApplicationWorkbenchWindowAdvisor.java
+++ trunk/org.neontoolkit.application/src/org/neontoolkit/application/intro/ApplicationWorkbenchWindowAdvisor.java
@@ -2,9 +2,10 @@ package org.neontoolkit.application.intro;
 
 import java.util.LinkedList;
 
-import org.eclipse.jface.action.ContributionManager;
+import org.eclipse.jface.action.CoolBarManager;
 import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.internal.provisional.action.ToolBarContributionItem2;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.MenuItem;
@@ -15,8 +16,10 @@ import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 import org.eclipse.ui.internal.ActionSetContributionItem;
+import org.eclipse.ui.internal.PluginActionCoolBarContributionItem;
 import org.eclipse.ui.internal.WorkbenchWindow;
 
+@SuppressWarnings("restriction")
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
     public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
@@ -26,95 +29,158 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
     public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer) {
         return new ApplicationActionBarAdvisor(configurer);
     }
-    @SuppressWarnings("restriction")
+    
 	@Override
     public void postWindowCreate() {
     	super.postWindowCreate();
     	if(this.getWindowConfigurer().getWindow() instanceof WorkbenchWindow){
-	    	MenuManager manager = ((WorkbenchWindow)this.getWindowConfigurer().getWindow()).getMenuBarManager();
-	    	IContributionItem hFile = manager.find("file");
-	    	if(hFile instanceof MenuManager){
-	    		MenuManager file = (MenuManager)hFile;
-	    		for(IContributionItem item : file.getItems()){
-	    			if(item != null && item.getId() != null){
-	    				if(item.getId().contains("LineDelimitersTo") || item.getId().contains("openLocalFile"))
-	        	    		item.setVisible(false);
-	    			}
-	    		}
-	    	}
-	    	IContributionItem hHelp = manager.find("help");
-	    	if(hHelp instanceof MenuManager){
-	    		MenuManager help = (MenuManager)hHelp;
-	    		for(IContributionItem item : help.getItems()){
-	    			if(item != null && item.getId() != null){
-	    				if(item.getId().contains("LineDelimitersTo") || item.getId().contains("openLocalFile"))
-	        	    		item.setVisible(false);
-	    			}
-	    		}
-	    	}
-//	    	IContributionItem hRun1 = manager.find("org.eclipse.ui.run");
-//	    	hRun1.setVisible(false);
-//	    	IContributionItem[] items = manager.getItems();
-//	    	LinkedList<IContributionItem> itemToRemove = new LinkedList<IContributionItem>();
-//	    	for(IContributionItem item : manager.getItems()){
-//	    		System.out.println(item.getId());
-//	    		if(item instanceof MenuManager && item.getId() != null){
-//	    			if(item.getId().contains("ExternalToolsSet") || item.getId().contains("org.eclipse.ui.run")){
-//	    				itemToRemove.add(item);
-//	    				item.setVisible(false);
-//	    			}
-//	    		}
-//	    		if(item instanceof ActionSetContributionItem && ((ActionSetContributionItem)item).getActionSetId() != null){
-//	    			if(((ActionSetContributionItem)item).getActionSetId().contains("ExternalToolsSet") || ((ActionSetContributionItem)item).getActionSetId().contains("org.eclipse.ui.run")){
-//	    				((ActionSetContributionItem)item).getInnerItem().setVisible(false);
-//	    				itemToRemove.add(item);
-//	    				item.setVisible(false);
-//	    			}
-//	    		}
-//	    		System.out.println("visible: " + item.isVisible());
-//	    		System.out.println("================");
-//	    	}
-//	    	for(IContributionItem item : itemToRemove)
-//	    		manager.remove(item);
-	    	LinkedList<MenuItem> menuItemToRemove = new LinkedList<MenuItem>();
-	    	for(MenuItem menuItem : manager.getMenu().getItems()){
-	    		Object data = menuItem.getData();
-	    		if(data instanceof MenuManager){
-	    			MenuManager item = (MenuManager)data;
-		    		if(item.getId() != null){
-		    			if(item.getId().contains("ExternalToolsSet") || item.getId().contains("org.eclipse.ui.run")){
-		    				menuItemToRemove.add(menuItem);
-		    				item.setVisible(false);
-		    			}
-		    		}
-	    		}
-	    		if(data instanceof ActionSetContributionItem){
-	    			ActionSetContributionItem item = (ActionSetContributionItem)data;
-		    		if(item.getActionSetId() != null){
-		    			if(((ActionSetContributionItem)item).getActionSetId().contains("ExternalToolsSet") || ((ActionSetContributionItem)item).getActionSetId().contains("org.eclipse.ui.run")){
-		    				((ActionSetContributionItem)item).getInnerItem().setVisible(false);
-		    				menuItemToRemove.add(menuItem);
-		    				item.setVisible(false);
-		    			}
-		    		}
-	    		}
-	    	}
-	    	for(MenuItem item : menuItemToRemove){
-	    		item.setEnabled(false);
-	    		item.getMenu().setVisible(false);
-	    	}
-	    	System.out.println("run2");
-    	}    		
+    		WorkbenchWindow window = (WorkbenchWindow)this.getWindowConfigurer().getWindow();
+	    	MenuManager manager = window.getMenuBarManager();
+	    	
+	    	removeItemsFromMenu(manager, "file", new String[]{"LineDelimitersTo","openLocalFile"});
+	    	removeItemsFromMenu(manager, "help", new String[]{"showKeyAssistHandler"});
+	    	
+	    	removeRunMenu(manager);
+
+	    	removeNavigationIconsFromCoolBar();
+    	}   
+
+    	PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().resetPerspective();
+    	
     }
-    public void preWindowOpen() {
+
+
+	public void preWindowOpen() {
+		
         IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-        configurer.setInitialSize(new Point(1000, 700));
-        configurer.setShowCoolBar(true);
+        configurer.setInitialSize(new Point(1200, 800));
+//        configurer.setShowCoolBar(true);
 		configurer.setShowPerspectiveBar(true);
-        configurer.setShowStatusLine(false);
-        configurer.setTitle(Messages.ApplicationWorkbenchWindowAdvisor_ApplicationTitle);
-        
+//        configurer.setShowStatusLine(false);
+//        configurer.setTitle(Messages.ApplicationWorkbenchWindowAdvisor_ApplicationTitle);
+//        
 		IPreferenceStore apiStore = PlatformUI.getPreferenceStore();
-		apiStore.setDefault(IWorkbenchPreferenceConstants.DOCK_PERSPECTIVE_BAR, "TOP_RIGHT"); 
+		apiStore.setDefault(IWorkbenchPreferenceConstants.DOCK_PERSPECTIVE_BAR, "TOP_RIGHT");
+		
+
+    	super.preWindowOpen();
+    	configurer.setShowStatusLine(true);
+        configurer.setShowProgressIndicator(true);
     }
+
+
+	/*
+	 * PRIVATE HELP METHODS to remove elements from the GUI
+	 */
+	
+	/**
+	 * removes the unneeded navigate and navigation Icons from the CoolBar
+	 */
+	private void removeNavigationIconsFromCoolBar() {
+    	CoolBarManager managerCoolBar = ((WorkbenchWindow)this.getWindowConfigurer().getWindow()).getCoolBarManager();
+    	for(IContributionItem item : managerCoolBar.getItems()){
+    		if(item instanceof ToolBarContributionItem2){
+    			ToolBarContributionItem2 tbcItem = (ToolBarContributionItem2)item;
+    			if(tbcItem.getId().equals("org.eclipse.ui.edit.text.actionSet.navigation")){
+    				item.setVisible(false);
+    			}
+    			if(tbcItem.getId().equals("org.eclipse.ui.workbench.navigate")){
+//    				tbcItem.setVisible(false);
+    				for(IContributionItem navigateItem : tbcItem.getToolBarManager().getItems()){
+    					if(navigateItem instanceof PluginActionCoolBarContributionItem){
+    						PluginActionCoolBarContributionItem ccItem = (PluginActionCoolBarContributionItem)navigateItem;
+	    					if(ccItem.getId().contains("gotoNextAnnotation")){
+	    						ccItem.setVisible(false);
+	    					} else if(ccItem.getId().contains("gotoPreviousAnnotation")){
+	    						ccItem.setVisible(false);
+//	    					} else if(ccItem.getId().contains("gotoLastEditPosition")){
+//	    						ccItem.setVisible(false);
+	    					}
+    					}
+    				}
+    			}
+    		}
+    	}
+		
+	}
+
+	/**
+	 * sets the items of itemNames in menu menuName invisible
+	 * @param manager - Manager of the menuBar
+	 * @param menuName - ID of the menu
+	 * @param itemNames - Identifying substring of the ID of the item
+	 */
+	private void removeItemsFromMenu(MenuManager manager, String menuName, String[] itemNames) {
+    	IContributionItem hFile = manager.find(menuName);
+    	if(hFile instanceof MenuManager){
+    		MenuManager file = (MenuManager)hFile;
+    		for(IContributionItem item : file.getItems()){
+    			if(item != null && item.getId() != null){
+    				for(String itemName : itemNames){
+    					if(item.getId().contains(itemName))
+            	    		item.setVisible(false);
+    				}
+    			}
+    		}
+    	}
+	}
+
+	/**
+	 * Method to remove the Run Menu based on:
+	 * 	* run menu itself
+	 * 	* External Tools Set
+	 * @param manager - Manager of the MenuBar
+	 */
+	private void removeRunMenu(MenuManager manager) {
+    	IContributionItem hRun1 = manager.find("org.eclipse.ui.run");
+		hRun1.setVisible(false);
+		
+		LinkedList<IContributionItem> itemToRemove = new LinkedList<IContributionItem>();
+		for(IContributionItem item : manager.getItems()){
+			if(item instanceof MenuManager && item.getId() != null){
+				if(item.getId().contains("ExternalToolsSet") || item.getId().contains("org.eclipse.ui.run")){
+					itemToRemove.add(item);
+					item.setVisible(false);
+				}
+			}
+			if(item instanceof ActionSetContributionItem && ((ActionSetContributionItem)item).getActionSetId() != null){
+				if(((ActionSetContributionItem)item).getActionSetId().contains("ExternalToolsSet") || ((ActionSetContributionItem)item).getActionSetId().contains("org.eclipse.ui.run")){
+					((ActionSetContributionItem)item).getInnerItem().setVisible(false);
+					itemToRemove.add(item);
+					item.setVisible(false);
+				}
+			}
+		}
+		for(IContributionItem item : itemToRemove)
+			manager.remove(item);
+		
+		LinkedList<MenuItem> menuItemToRemove = new LinkedList<MenuItem>();
+		for(MenuItem menuItem : manager.getMenu().getItems()){
+			Object data = menuItem.getData();
+			if(data instanceof MenuManager){
+				MenuManager item = (MenuManager)data;
+	    		if(item.getId() != null){
+	    			if(item.getId().contains("ExternalToolsSet") || item.getId().contains("org.eclipse.ui.run")){
+	    				menuItemToRemove.add(menuItem);
+	    				item.setVisible(false);
+	    			}
+	    		}
+			}
+			if(data instanceof ActionSetContributionItem){
+				ActionSetContributionItem item = (ActionSetContributionItem)data;
+	    		if(item.getActionSetId() != null){
+	    			if(((ActionSetContributionItem)item).getActionSetId().contains("ExternalToolsSet") || ((ActionSetContributionItem)item).getActionSetId().contains("org.eclipse.ui.run")){
+	    				((ActionSetContributionItem)item).getInnerItem().setVisible(false);
+	    				menuItemToRemove.add(menuItem);
+	    				item.setVisible(false);
+	    			}
+	    		}
+			}
+		}
+		for(MenuItem item : menuItemToRemove){
+			item.setEnabled(false);
+			item.getMenu().setVisible(false);
+		}
+		
+	}
 }
