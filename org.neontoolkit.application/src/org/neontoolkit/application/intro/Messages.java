d26d62aa6311f52c613c6f81e35d96a85175c162
nicostieler <nicostieler@641896ee-dca0-4984-aaae-e2f8ba423eba>
2010-11-24 14:11:00 +0000

    bug fixed: bug #450 reimplementation of prompt for workspace when the Neon Toolkit is starting

--- org.neontoolkit.application/src/org/neontoolkit/application/intro/Messages.java
+++ org.neontoolkit.application/src/org/neontoolkit/application/intro/Messages.java
@@ -6,6 +6,18 @@ public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.neontoolkit.application.intro.messages"; //$NON-NLS-1$
 	public static String ApplicationWorkbenchAdvisor_StartPerspective;
 	public static String ApplicationWorkbenchWindowAdvisor_ApplicationTitle;
+	public static String Application_configurationFile_line1;
+	public static String Application_configurationFile_line2;
+	public static String Application_configurationFile_line3;
+	public static String Application_configurationFile_line4;
+	public static String Application_configurationFile_recentWorkspace;
+	public static String Application_configurationFile_recentWorkspace_seperator;
+	public static String Application_configurationFile_directory;
+	public static String Application_configurationFile_connector;
+	public static String Application_configurationFile_configFileName;
+	public static String Application_datePattern;
+	public static String Application_defaultWorkspace;
+
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
