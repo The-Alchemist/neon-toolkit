3eab124b2fe7eab25aef6feae6d0acb74a472aea
NicoStieler <NicoStieler@641896ee-dca0-4984-aaae-e2f8ba423eba>
2011-02-14 13:57:39 +0000

    Release NTK 2.4.1

---
+++ tags/NTK_2.4.1/com.ontoprise.ontostudio.owl.visualize/src/main/com/ontoprise/ontostudio/owl/visualize/nodes/IndividualMultiNode.java
@@ -0,0 +1,47 @@
+/*****************************************************************************
+ * Copyright (c) 2009 ontoprise GmbH.
+ *
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+
+package com.ontoprise.ontostudio.owl.visualize.nodes;
+
+import org.eclipse.core.runtime.CoreException;
+import org.semanticweb.owlapi.model.OWLEntity;
+
+import com.ontoprise.ontostudio.owl.gui.OWLPlugin;
+import com.ontoprise.ontostudio.owl.gui.OWLSharedImages;
+import com.ontoprise.ontostudio.owl.visualize.Messages;
+
+
+/*
+ * Created by Werner Hihn
+ */
+
+public class IndividualMultiNode extends IndividualNode {
+
+    @Override
+    public String getNodeType() {
+        return Messages.IndividualMultiNode_0;
+    }
+    
+    public IndividualMultiNode() {
+        super();
+        setImage(OWLPlugin.getDefault().getImageRegistry().get(OWLSharedImages.INDIVIDUAL));
+    }
+
+    public IndividualMultiNode(OWLEntity entity, String ontologyId, String projectId) {
+        super(entity, ontologyId, projectId);
+        setImage(OWLPlugin.getDefault().getImageRegistry().get(OWLSharedImages.INDIVIDUAL));
+    }
+    
+    @Override
+    public Object create() throws CoreException {
+        return new IndividualMultiNode();
+    }
+
+}
