8575b73cccd97d98690dd790561f088cb06df9fa
NicoStieler <NicoStieler@641896ee-dca0-4984-aaae-e2f8ba423eba>
2011-04-01 08:35:39 +0000

    removed null Pointer Exception in IndividualView while start up NTK

--- trunk/com.ontoprise.ontostudio.owl.gui/src/main/com/ontoprise/ontostudio/owl/gui/individualview/ShowIndividualsOfAllSubclasses.java
+++ trunk/com.ontoprise.ontostudio.owl.gui/src/main/com/ontoprise/ontostudio/owl/gui/individualview/ShowIndividualsOfAllSubclasses.java
@@ -3,11 +3,10 @@
  */
 package com.ontoprise.ontostudio.owl.gui.individualview;
 
-import org.eclipse.jface.action.IAction;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.neontoolkit.gui.NeOnUIPlugin;
 
 import com.ontoprise.ontostudio.owl.gui.OWLPlugin;
@@ -16,29 +15,15 @@ import com.ontoprise.ontostudio.owl.gui.OWLPlugin;
  * @author Nico Stieler
  * Created on: 29.03.2011
  */
-public class ShowIndividualsOfAllSubclasses  implements IWorkbenchWindowActionDelegate {
+public class ShowIndividualsOfAllSubclasses extends AbstractHandler {
 
     @Override
-    public void dispose() {
-    }
-
-    @Override
-    public void init(IWorkbenchWindow window) {
-        IPreferenceStore store = NeOnUIPlugin.getDefault().getPreferenceStore();
-        store.setValue(OWLPlugin.SHOW_INSTANCES_OF_ALL_SUBCLASSES_PREFERENCE, false);
-    }
-
-    @Override
-    public void run(IAction action) {
+    public Object execute(ExecutionEvent arg0) throws ExecutionException {
         IPreferenceStore store = NeOnUIPlugin.getDefault().getPreferenceStore();
         Boolean oldValue = store.getBoolean(OWLPlugin.SHOW_INSTANCES_OF_ALL_SUBCLASSES_PREFERENCE);
         Boolean newValue = !oldValue;
         store.setValue(OWLPlugin.SHOW_INSTANCES_OF_ALL_SUBCLASSES_PREFERENCE, newValue);
         store.firePropertyChangeEvent(NeOnUIPlugin.ID_DISPLAY_PREFERENCE, NeOnUIPlugin.DISPLAY_LOCAL, OWLPlugin.DISPLAY_LANGUAGE);
-    }
-
-    @Override
-    public void selectionChanged(IAction arg0, ISelection arg1) {
-        
+        return null;
     }
 }
