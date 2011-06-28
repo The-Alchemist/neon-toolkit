277370996ef5638829bb5b3135a208e6f2da5380
NicoStieler <NicoStieler@641896ee-dca0-4984-aaae-e2f8ba423eba>
2011-06-28 12:12:28 +0000

    feature added
    bug 521
    
    "selected resource" is selected, if the search page is opened via the pop up menu

--- com.ontoprise.ontostudio.owl.gui/src/main/com/ontoprise/ontostudio/search/owl/ui/SearchFromPopupHandler.java
+++ com.ontoprise.ontostudio.owl.gui/src/main/com/ontoprise/ontostudio/search/owl/ui/SearchFromPopupHandler.java
@@ -6,6 +6,7 @@ package com.ontoprise.ontostudio.search.owl.ui;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.search.internal.ui.OpenSearchDialogAction;
+import org.eclipse.search.ui.ISearchPageContainer;
 import org.eclipse.ui.IWorkbenchPart;
 import org.neontoolkit.gui.navigator.actions.AbstractSelectionBasedHandler;
 
@@ -19,8 +20,9 @@ public class SearchFromPopupHandler  extends AbstractSelectionBasedHandler {
     @Override
     public Object executeWithSelection(IWorkbenchPart part, IStructuredSelection selection) throws ExecutionException {
         OpenSearchDialogAction action = new OpenSearchDialogAction();
-//        SearchPlugin.get
+        OwlSearchPage.setPresetScopeValue(ISearchPageContainer.SELECTION_SCOPE);
         action.run();
+        OwlSearchPage.activatePresetScope(false);
         return null;
     }
 
