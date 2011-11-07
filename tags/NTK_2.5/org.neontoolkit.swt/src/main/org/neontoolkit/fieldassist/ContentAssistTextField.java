b72d94e64a2f69e9b7e7de4c01bea42e5f2d694f
nicostieler <nicostieler@641896ee-dca0-4984-aaae-e2f8ba423eba>
2011-11-07 09:32:39 +0000

    tag 2.5

---
+++ tags/NTK_2.5/org.neontoolkit.swt/src/main/org/neontoolkit/fieldassist/ContentAssistTextField.java
@@ -0,0 +1,162 @@
+/*****************************************************************************
+ * Copyright (c) 2009 ontoprise GmbH.
+ *
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+
+package org.neontoolkit.fieldassist;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import org.eclipse.jface.dialogs.PopupDialog;
+import org.eclipse.jface.fieldassist.ContentProposalAdapter;
+import org.eclipse.jface.fieldassist.IContentProposal;
+import org.eclipse.jface.fieldassist.IContentProposalProvider;
+import org.eclipse.jface.fieldassist.TextContentAdapter;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.swt.events.VerifyEvent;
+import org.eclipse.swt.events.VerifyListener;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.fieldassist.ContentAssistCommandAdapter;
+
+@SuppressWarnings("deprecation") 
+public class ContentAssistTextField extends org.eclipse.ui.fieldassist.ContentAssistField {
+	
+	private VerifyListener _verifyListener;
+	
+	public ContentAssistTextField(Composite parent, int style, IContentProposalProvider proposalProvider, String commandId, char[] autoActivationCharacters) {
+		super(
+				parent, 
+				style, 
+				new org.eclipse.jface.fieldassist.IControlCreator() {
+					public Control createControl(Composite parent, int style) {
+						return new Text(parent, style);
+					}
+				},
+				new DummyContentAdapter(),
+				proposalProvider, 
+				commandId, 
+				autoActivationCharacters);
+		((DummyContentAdapter)getContentAssistCommandAdapter().getControlContentAdapter()).setTextContentAdapter(new TextContentAdapter() {
+				@Override
+				public void insertControlContents(Control control, String text, int cursorPosition) {
+					setText(text);
+					finishEditing();
+				}
+			});
+		getContentAssistCommandAdapter().setFilterStyle(ContentAssistCommandAdapter.FILTER_NONE);
+		getContentAssistCommandAdapter().setAutoActivationCharacters(null);
+		getContentAssistCommandAdapter().setProposalAcceptanceStyle(ContentProposalAdapter.PROPOSAL_INSERT);
+
+		
+	}
+	public void finishEditing() {
+		closePopup(false);
+	}
+	
+	public void cancelEditing() {
+		closePopup(false);
+	}
+	
+	public void setSelectedItem() {
+    	closePopup(true);
+	}
+	
+	private PopupDialog getPopup() {
+		try {
+			Field field = ContentProposalAdapter.class.getDeclaredField("popup"); //$NON-NLS-1$
+			field.setAccessible(true);
+			return (PopupDialog)field.get(getContentAssistCommandAdapter());
+		} catch (NoSuchFieldException nsfe) {
+		} catch (IllegalAccessException iae) {
+		}
+		return null;
+	}
+	
+	/**
+	 * Forces the popup to close. If store is <code>true</code>, the curent selected element is stored
+	 * as selection, otherwise none selection is stored.
+	 * @param store parameter indicating whether the current selection should be stored.
+	 */
+	public void closePopup(boolean store) {
+		try {
+			PopupDialog popup = getPopup();
+			if (popup != null) {
+				if (store) {
+					Class<?>[] classes = ContentProposalAdapter.class.getDeclaredClasses();
+					Method method = classes[0].getDeclaredMethod("acceptCurrentProposal",new Class[]{}); //$NON-NLS-1$
+					method.setAccessible(true);
+					method.invoke(popup, new Object[]{});
+				}
+				else {
+					popup.close();
+				}
+				getControl().setFocus();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Text getTextControl() {
+		return (Text)getControl();
+	}
+	
+	/**
+	 * Sets the label provider for the completion popup
+	 * @param lableProvider
+	 */
+	public void setLabelProvider(ILabelProvider labelProvider) {
+		getContentAssistCommandAdapter().setLabelProvider(labelProvider);
+	}
+	
+	private String getText(String inserted) {
+		StringBuffer buf = new StringBuffer(getTextControl().getText());
+		Point selection = getTextControl().getSelection();
+		if (selection.x == selection.y) {
+			buf.insert(getTextControl().getCaretPosition(), inserted);
+		}
+		else {
+			buf.replace(selection.x, selection.y, inserted);
+		}
+		return buf.toString();
+	}
+
+	private IContentProposal[] getProposals(String contents) {
+		IContentProposal[] proposals = getContentAssistCommandAdapter().getContentProposalProvider().getProposals(contents, getTextControl().getCaretPosition());
+		return proposals;
+	}
+
+	public void showProposalsOnly(boolean proposalsOnly) {
+		if (_verifyListener == null) {
+			_verifyListener = new VerifyListener(){
+				public void verifyText(VerifyEvent e) {
+					if (getProposals(getText(e.text)).length == 0) {
+						e.doit = false;
+					}
+				}
+			};
+		}
+		if (proposalsOnly) {
+			getTextControl().addVerifyListener(_verifyListener);
+		}
+		else {
+			getTextControl().removeVerifyListener(_verifyListener);
+		}
+	}
+	
+	public void setText(String text) {
+		char[] aac = getContentAssistCommandAdapter().getAutoActivationCharacters();
+		getContentAssistCommandAdapter().setAutoActivationCharacters(new char[0]);
+		getTextControl().setText(text);
+		getContentAssistCommandAdapter().setAutoActivationCharacters(aac);
+	}
+}
