1f0b0e325aec6b131189031d9a9ae8ddadd7b062
nicostieler <nicostieler@641896ee-dca0-4984-aaae-e2f8ba423eba>
2011-11-07 09:29:22 +0000

    tag 2.5

---
+++ tags/NTK_2.5/org.neontoolkit.core/src/main/org/neontoolkit/core/project/IOntologyProjectFilter.java
@@ -0,0 +1,18 @@
+/*****************************************************************************
+ * Copyright (c) 2009 ontoprise GmbH.
+ *
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+
+package org.neontoolkit.core.project;
+
+/**
+ *
+ */
+public interface IOntologyProjectFilter {
+    boolean matches(IOntologyProject ontologyProject);
+}
---
+++ tags/NTK_2.5/org.neontoolkit.core/src/main/org/neontoolkit/core/project/IProjectFailureListener.java
@@ -0,0 +1,30 @@
+/*****************************************************************************
+ * Copyright (c) 2009 ontoprise GmbH.
+ *
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+
+package org.neontoolkit.core.project;
+
+/**
+ * Additional interface for listeners that want to react on problems of
+ * IOntologyProjects.
+ * @author Dirk Wenke
+ */
+public interface IProjectFailureListener {
+    
+    /**
+     * Listener method notifying of a project failure. The project and the exception
+     * that occured are passed as arguments.
+     * If the failure is only temporary, this method might be called again with the exception
+     * parameter being null. This means that the project is valid again.
+     * @param project
+     * @param exception
+     */
+    void projectFailureOccured(String project, Exception exception);
+
+}
